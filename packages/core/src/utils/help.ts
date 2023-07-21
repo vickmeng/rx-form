eb5ad350cbe54a822385c46812e68e20486afdf3
mengweikang <mengweikang@yzw.cn>
2023-07-21 13:59:02 +0800

    docs: 添加备注

--- packages/core/src/utils/help.ts
+++ packages/core/src/utils/help.ts
@@ -16,6 +16,10 @@ export const getErrorsBy = (control: AbstractControl, validators: ValidatorFn[])
   return isEmpty(errors) ? null : errors;
 };
 
+/**
+ * createControl
+ * Accept the Control instance as input and return it itself; otherwise, accept the parameters to construct a FieldControl and return the constructed FieldControl
+ */
 export const createControl = (params: CreateControlParams) => {
   if (params instanceof AbstractControl) {
     return params;
