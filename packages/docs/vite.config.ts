ac22e4c6061289f544463a1db4e6580f027bfb65
mengweikang <mengweikang@yzw.cn>
2023-09-04 16:18:44 +0800

    feat: 调整发布

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build:{
     outDir:'../../docs',
-    assetsDir:'rx-form'
+    assetsDir:'rx-form/assets'
   }
   // preprocessorOptions: {
   //   less: {
