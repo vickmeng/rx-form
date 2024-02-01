e424af881746d5c06994da411ac4337903aee81a
mengweikang <mengweikang@yzw.cn>
2024-02-01 14:26:40 +0800

    feat: deepCheckFirstInvalidControl

---
+++ packages/core/src/__test__/help.test.ts
@@ -0,0 +1,70 @@
+import { deepCheckFirstInvalidControl } from '../utils';
+import { FieldControl } from '../controls/fieldControl';
+import { requiredValidatorFactory } from '../validators/validatorMessageFactories';
+import { GroupControl } from '../controls/groupControl';
+import { ListControl } from '../controls/listControl';
+
+describe('help', () => {
+  describe('deepCheckFirstInvalidControl', () => {
+    it('should get null when deepCheckFirstInvalidControl called given valid FieldControl', () => {
+      const fieldControl = new FieldControl('1', {
+        validators: [requiredValidatorFactory({ message: '必填' })],
+      });
+
+      expect(deepCheckFirstInvalidControl([fieldControl])).toBeUndefined();
+    });
+
+    it('should get invalid control when deepCheckFirstInvalidControl called given invalid FieldControl', () => {
+      const fieldControl = new FieldControl(undefined, {
+        validators: [requiredValidatorFactory({ message: '必填' })],
+      });
+
+      expect(deepCheckFirstInvalidControl([fieldControl])).toBe(fieldControl);
+    });
+
+    it('should get invalid control when deepCheckFirstInvalidControl called given invalid GroupControl', () => {
+      const groupControl = new GroupControl({
+        a: ['123', { validators: [requiredValidatorFactory({ message: '必填' })] }],
+        b: ['', { validators: [requiredValidatorFactory({ message: '必填' })] }],
+      });
+
+      expect(deepCheckFirstInvalidControl([groupControl])).toBe(groupControl.get('b'));
+    });
+
+    it('should get null when deepCheckFirstInvalidControl called given valid ListControl', () => {
+      const listControl = new ListControl([
+        ['123', { validators: [requiredValidatorFactory({ message: '必填' })] }],
+        ['123', { validators: [requiredValidatorFactory({ message: '必填' })] }],
+      ]);
+
+      expect(deepCheckFirstInvalidControl([listControl])).toBeUndefined();
+    });
+
+    it('should get invalid control when deepCheckFirstInvalidControl called given invalid ListControl', () => {
+      const listControl = new ListControl([
+        ['123', { validators: [requiredValidatorFactory({ message: '必填' })] }],
+        ['', { validators: [requiredValidatorFactory({ message: '必填' })] }],
+      ]);
+
+      expect(deepCheckFirstInvalidControl([listControl])).toBe(listControl.get(1));
+    });
+
+    it('should get invalid control when deepCheckFirstInvalidControl called given complex case', () => {
+      const groupControl = new GroupControl({
+        '0': ['123', { validators: [requiredValidatorFactory({ message: '必填' })] }],
+        '1': new GroupControl({
+          '1-0': ['123', { validators: [requiredValidatorFactory({ message: '必填' })] }],
+          '1-1': new ListControl([
+            ['123', { validators: [requiredValidatorFactory({ message: '必填' })] }],
+            ['', { validators: [requiredValidatorFactory({ message: '必填' })] }],
+          ]),
+        }),
+      });
+
+      // @ts-ignore-next-line
+      const errorControl = groupControl.get('1').get('1-1').get(1);
+
+      expect(deepCheckFirstInvalidControl([groupControl])).toBe(errorControl);
+    });
+  });
+});
--- packages/core/src/utils/help.ts
+++ packages/core/src/utils/help.ts
@@ -3,6 +3,8 @@ import { isEmpty } from 'lodash';
 import { CreateControlParams, Errors, ValidatorFn } from '../types';
 import { AbstractControl } from '../controls/abstractControl';
 import { FieldControl } from '../controls/fieldControl';
+import { GroupControl } from '../controls/groupControl';
+import { ListControl } from '../controls/listControl';
 
 export const getErrorsBy = (control: AbstractControl, validators: ValidatorFn[]) => {
   const errors: Errors = validators.reduce((acc, cur) => {
@@ -26,4 +28,27 @@ export const createControl = (params: CreateControlParams) => {
   } else {
     return new FieldControl(...params);
   }
+};
+/**
+ * 深度递归深度寻找control以及其子control的第一个有error的control
+ */
+export const deepCheckFirstInvalidControl = (controls: AbstractControl[]) => {
+  let res;
+  const loop = (_controls: AbstractControl[]) => {
+    _controls.find((_control) => {
+      if (!isEmpty(_control.errors)) {
+        res = _control;
+        return true;
+      } else if (_control instanceof GroupControl) {
+        return loop(Object.values(_control.controls));
+      } else if (_control instanceof ListControl) {
+        return loop(_control.controls);
+      } else {
+        return false;
+      }
+    });
+  };
+  loop(controls);
+
+  return res;
 };
