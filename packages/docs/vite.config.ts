9eea8ce58d02d2a6485a2e4d223cd9ec78140271
mengweikang <mengweikang@yzw.cn>
2025-01-15 18:59:12 +0800

    docs

--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../../docs', // 将此路径修改为你想要的输出目录
+  },
   // preprocessorOptions: {
   //   less: {
   //     math: 'parens-division',
